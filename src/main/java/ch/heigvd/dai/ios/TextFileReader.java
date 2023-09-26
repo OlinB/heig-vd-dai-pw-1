a78799a905f8e4e1b1ff8dbbdf650d7de10810f5
olinb <olin.bourquin@gmail.com>
2023-09-26 17:33:31 +0200

    add reader and writer classes
    Signed-off-by: olinb <olin.bourquin@gmail.com>

---
+++ src/main/java/ch/heigvd/dai/ios/TextFileReader.java
@@ -0,0 +1,9 @@
+package ch.heigvd.dai.ios;
+
+public class TextFileReader {
+
+    public void read(String filename, String encoding) {
+        // TODO : implement this method
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ src/main/java/ch/heigvd/dai/ios/TextFileWriter.java
@@ -0,0 +1,9 @@
+package ch.heigvd.dai.ios;
+
+public class TextFileWriter {
+
+    public void write(String filename, String content, String encoding) {
+        // TODO : implement this method
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
