530024562d25509f670797e53a4166b3fd64671a
OlinB <olin.bourquin@gmail.com>
2023-10-03 13:44:25 +0200

    Add reader and writer function scaffolding

--- src/main/java/ch/heigvd/dai/ios/TextFileReader.java
+++ src/main/java/ch/heigvd/dai/ios/TextFileReader.java
@@ -1,9 +1,21 @@
 package ch.heigvd.dai.ios;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
 public class TextFileReader {
 
-    public void read(String filename, String encoding) {
-        // TODO : implement this method
+    public String read(String filename, Charset encoding) {
+        try(Reader reader = new FileReader(filename, encoding)){
+            return reader.toString();
+        } catch (FileNotFoundException e) {
+            // TODO
+        } catch (IOException e) {
+            // TODO
+        }
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }
--- src/main/java/ch/heigvd/dai/ios/TextFileWriter.java
+++ src/main/java/ch/heigvd/dai/ios/TextFileWriter.java
@@ -1,9 +1,21 @@
 package ch.heigvd.dai.ios;
 
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class TextFileWriter {
 
-    public void write(String filename, String content, String encoding) {
-        // TODO : implement this method
+    public void write(String filename, String content, Charset encoding) {
+        try(Writer writer = new FileWriter(filename, encoding)){
+            writer.write(content.toString());
+        } catch (FileNotFoundException e) {
+            // TODO
+        } catch (IOException e) {
+            // TODO
+        }
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }
