99ac6eced9472f8147d61116e827804c48b37b67
Kevin Farine <kevin.farine@hotmail.com>
2023-10-09 16:46:31 +0200

    Ajout de la fonction dans le cli. Echec pour faire le .jar

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -10,17 +10,19 @@ import java.util.concurrent.Callable;
 
 @Command(name = "replaceWord", mixinStandardHelpOptions = true, version = "replaceWord 0.1",
         description = "Replace all occurence of specific work in a file text and create new file")
-class ReplaceWord implements Callable<Integer{
+class ReplaceWord implements Callable<Integer>{
     @Option(names = {"-i", "--if","--inputFile"}, paramLabel = "ARCHIVE",
             description = "input file whose the specific word will be searched")
-    private File inputFile;
+    //private File inputFile;
+    private String inputFile;
 
     @Option(names = {"--ie", "--inputEncoding"}, defaultValue = "UTF-8", description = "Encoding of the input file")
     private String inputEncoding;
 
     @Option(names = {"-o", "--of", "--outputFile"}, paramLabel = "FILE",
             description = "output file whose will be created with the word replaced")
-    private File outputFile;
+    //private File outputFile;
+    private String outputFile;
 
     @Option(names = {"--oe", "--outputEncoding"}, defaultValue = "UTF-8", description = "Encoding of the output file")
     private String outputEncoding;
@@ -38,12 +40,13 @@ class ReplaceWord implements Callable<Integer{
 
     @Override
     public Integer call() throws Exception{
+        ch.heigvd.dai.string.ReplaceWord.replaceInFile(inputFile, outputFile, searchedWord, replacedWord);
         return 0;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new CommandLine(new Main()).execute(args);
     }
 }
